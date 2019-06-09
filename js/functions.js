84c5996f04d4918eb0d933f1b754db56a71c0488
BrentPF <Brent.Flores@hotmail.com>
2019-06-09 12:55:11 -0400

    Cleaned code and added visual upgrades

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,51 @@
+var links = ["About", "Portfolio", "Contact"];
 var typeSpeed = 100;
-var isPrinting = false;
-var line;
-window.onload = function() {
-    line = document.getElementById("About");
-    moveRight(document.getElementById("screen"));
-  };
+var transitionTime = 1500;
+var signature;
+var screen;
+var screenText;
 
-function resizeScreen(){
-    document.getElementById("screen").style.height = "80%";
-}
+window.onload = function() {
+    signature = document.getElementById("Name");
+    screen = document.getElementById("screen");
+    screenText = document.getElementById("screenText");
+    moveRight(screen);
+  };
 
 function moveRight(screen) {
     screen.style.left = "50%";
-    setTimeout(growDown, 1500, screen);
+    setTimeout(growDown, transitionTime, screen);
 }
 
 function growDown(screen){
     screen.style.height = "80%";
-    setTimeout(screenText, 1500, document.getElementById("screenText"));
+    setTimeout(printText, transitionTime, screenText);
 }
 
-function screenText(screenBody){
+function printText(screenBody){
+    screen.style.overflow = "auto";
     screenBody.style.color = "black";
-    //document.getElementById("Name").innerText = "> Brent P.F.";
     screenBody.style.color = "white";
-    //screenBody.innerText = screenBody.innerText + "\n>\xa0";
-    //var line = document.getElementById("About");
-    //line.innerHTML = "bruh";
-    setTimeout(printName, typeSpeed, document.getElementById("Name"), ">\xa0Brent\xa0P.F.", 0);
+    setTimeout(printName, typeSpeed, signature, ">\xa0Brent\xa0P.F.", 0);
+}
+//id is "About" etc.
+function printLinks(links){
+    var container = document.getElementById(links[0]);
+    var index = 0;
+    container.innerText = ">\xa0";
+    setTimeout(printChar, typeSpeed, container, links[0], index, links, 0);
+    
+}
+
+function printChar(container, id, index, links, linksIndex){
+    if (index < id.length){
+        container.innerHTML = container.innerHTML + id.charAt(index);
+        setTimeout(printChar, typeSpeed, container, id, index+1, links, linksIndex);
+    } else if (linksIndex+1 < links.length){
+        var nextLink = document.getElementById(links[linksIndex+1]);
+        nextLink.innerText = ">\xa0";
+        setTimeout(printChar, typeSpeed, nextLink, links[linksIndex+1], 0, links, linksIndex+1);
+    }
 }
 
 function printName(screenBody, text, index){
@@ -35,34 +53,6 @@ function printName(screenBody, text, index){
         screenBody.innerText = screenBody.innerText + text.charAt(index);
         setTimeout(printName, typeSpeed, screenBody, text, index+1);
     } else {
-        document.getElementById("About").innerText = ">\xa0";
-        setTimeout(printAbout, typeSpeed, line, "About", 0);
-    }
-}
-
-function printAbout(screenBody, text, index){
-    if (index < text.length){
-        screenBody.innerHTML = screenBody.innerHTML + text.charAt(index);
-        setTimeout(printAbout, typeSpeed, screenBody, text, index+1);
-    } else {
-        document.getElementById("Portfolio").innerText = ">\xa0";
-        setTimeout(printPortfolio, typeSpeed, document.getElementById("Portfolio"), "Portfolio", 0);
-    }
-}
-
-function printPortfolio(screenBody, text, index){
-    if (index < text.length){
-        screenBody.innerHTML = screenBody.innerHTML + text.charAt(index);
-        setTimeout(printPortfolio, typeSpeed, screenBody, text, index+1);
-    } else {
-        document.getElementById("Contact").innerText = ">\xa0";
-        setTimeout(printContact, typeSpeed, document.getElementById("Contact"), "Contact", 0);
-    }
-}
-
-function printContact(screenBody, text, index){
-    if (index < text.length){
-        screenBody.innerHTML = screenBody.innerHTML + text.charAt(index);
-        setTimeout(printContact, typeSpeed, screenBody, text, index+1);
+        printLinks(links);
     }
 }
